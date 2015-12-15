8b11c9417a8c89eba57fef459e796b90e3149ce1
Josh Klun <jklun@nerdery.com>
2015-12-14 20:54:27 -0600

    Add basic test cases.

---
+++ src/test/scala/com/nerdery/prisonersdilemma/PrisonerSpec.scala
@@ -0,0 +1,62 @@
+package com.nerdery.prisonersdilemma
+
+import org.scalatest.{Matchers, FreeSpec}
+
+/**
+  * A test that the basic Rat and Silent prisoner types behave properly in all cases.
+  *
+  * @author Josh Klun (jklun@nerdery.com)
+  */
+class PrisonerSpec extends FreeSpec with Matchers {
+  "A Silent Prisoner" - {
+    val prisoner = new SilentPrisoner
+    def silentCheck(interrogation: Interrogation) = prisoner.doesConfess(interrogation) should be(false)
+    def silentValidation(check: Interrogation => Unit, interrogation: Interrogation) = "remain silent" in { check(interrogation) }
+
+    "with no prior results" - {
+      silentValidation(silentCheck, Interrogation("jdoe", "jvm", None, None))
+    }
+
+    "with matching prior confessions" - {
+      silentValidation(silentCheck, Interrogation("jdoe", "jvm", Some(true), Some(true)))
+    }
+
+    "with matching prior silence" - {
+      silentValidation(silentCheck, Interrogation("jdoe", "jvm", Some(true), Some(true)))
+    }
+
+    "with prior partner betrayal" - {
+      silentValidation(silentCheck, Interrogation("jdoe", "jvm", Some(false), Some(true)))
+    }
+
+    "with prior player betrayal" - {
+      silentValidation(silentCheck, Interrogation("jdoe", "jvm", Some(true), Some(false)))
+    }
+  }
+
+  "A Rat Prisoner" - {
+    val prisoner = new RatPrisoner
+    def confessionCheck(interrogation: Interrogation) = prisoner.doesConfess(interrogation) should be(true)
+    def confessionValidation(check: Interrogation => Unit, interrogation: Interrogation) = "confess" in { check(interrogation) }
+
+    "with no prior results" - {
+      confessionValidation(confessionCheck, Interrogation("jdoe", "jvm", None, None))
+    }
+
+    "with matching prior confessions" - {
+      confessionValidation(confessionCheck, Interrogation("jdoe", "jvm", Some(true), Some(true)))
+    }
+
+    "with matching prior silence" - {
+      confessionValidation(confessionCheck, Interrogation("jdoe", "jvm", Some(true), Some(true)))
+    }
+
+    "with prior partner betrayal" - {
+      confessionValidation(confessionCheck, Interrogation("jdoe", "jvm", Some(false), Some(true)))
+    }
+
+    "with prior player betrayal" - {
+      confessionValidation(confessionCheck, Interrogation("jdoe", "jvm", Some(true), Some(false)))
+    }
+  }
+}
